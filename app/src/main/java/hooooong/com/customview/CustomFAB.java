61b6ed5a09814a2f0e349762ff2f2ae9538370d7
Hooooong <h921013@gmail.com>
2017-10-07 22:51:35 +0900

    FAB Animation, Snackbar 추가

--- app/src/main/java/hooooong/com/customview/CustomFAB.java
+++ app/src/main/java/hooooong/com/customview/CustomFAB.java
@@ -7,6 +7,7 @@ import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 
 /**
  * Created by Android Hong on 2017-10-07.
@@ -18,43 +19,30 @@ public class CustomFAB extends FloatingActionButton.Behavior {
         super();
     }
 
+    @Override
     public boolean onStartNestedScroll(CoordinatorLayout parent, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
         return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
-    }
-
-    @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
-        if(dependency instanceof RecyclerView)
-            return true;
-
-        return false;
     }
 
     @Override
     public void onNestedScroll(CoordinatorLayout coordinatorLayout,
                                FloatingActionButton child, View target, int dxConsumed,
                                int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-        // TODO Auto-generated method stub
-        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed,
-                dxUnconsumed, dyUnconsumed);
-
-        System.out.println("dxConsumed : " + dxConsumed );
-        System.out.println("dyConsumed : " + dyConsumed );
-        System.out.println("dxUnconsumed : " + dxUnconsumed );
-        System.out.println("dyUnconsumed : " + dyUnconsumed );
-
-        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+        //child -> Floating Action Button
+        /*if (child.getVisibility() == View.VISIBLE && dyConsumed > 0) {
             child.hide();
-        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+        } else if (child.getVisibility() == View.GONE && dyConsumed < 0) {
             child.show();
+        }*/
+
+        //child -> Floating Action Button
+        if (dyConsumed > 0) {
+            CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
+            int fab_bottomMargin = layoutParams.bottomMargin;
+            child.animate().translationY(child.getHeight() + fab_bottomMargin).setInterpolator(new LinearInterpolator()).start();
+        } else if (dyConsumed < 0) {
+            child.animate().translationY(0).setInterpolator(new LinearInterpolator()).start();
         }
     }
 
-    @Override
-    public void onStopNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target) {
-        super.onStopNestedScroll(coordinatorLayout, child, target);
-        if(child.getVisibility() != View.VISIBLE){
-            child.show();
-        }
-    }
 }
--- app/src/main/java/hooooong/com/customview/CustomFABActivity.java
+++ app/src/main/java/hooooong/com/customview/CustomFABActivity.java
@@ -1,9 +1,13 @@
 package hooooong.com.customview;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +15,7 @@ import java.util.List;
 public class CustomFABActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +23,26 @@ public class CustomFABActivity extends AppCompatActivity {
         setContentView(R.layout.activity_custom_fab);
 
         initView();
+        initListener();
+    }
+
+    private void initListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "클릭", 10000).setAction("YES", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }).show();
+
+            }
+        });
     }
 
     private void initView(){
+        fab = (FloatingActionButton)findViewById(R.id.fab);
         recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         CustomAdapter customAdapter = new CustomAdapter();
